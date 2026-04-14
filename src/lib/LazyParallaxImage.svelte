5b40fbd865033a2664992429d11031ed999cbed0
chadsteele <chad@chadsteele.com>
2026-04-14 08:37:43 +0400

    Add kenBurns animation to lazy-parallax-image and adjust background size

--- src/lib/LazyParallaxImage.svelte
+++ src/lib/LazyParallaxImage.svelte
@@ -5,13 +5,32 @@
 <div class="lazy-parallax-image" style="background-image: url('{src}');" />
 
 <style>
+	@keyframes kenBurns {
+		from {
+			background-size: 120%;
+			background-position: center;
+		}
+		to {
+			background-size: 150%;
+			background-position: center;
+		}
+	}
+
 	.lazy-parallax-image {
 		width: 100%;
 		height: 20vh;
 		border-radius: 12px;
-		background-size: cover;
+		background-size: contain;
 		background-position: center;
 		background-repeat: no-repeat;
-		background-attachment: fixed;
+		animation: kenBurns 8s ease-in-out infinite alternate;
+	}
+
+	/* Parallax for devices 600px+ */
+	@media (min-width: 600px) {
+		.lazy-parallax-image {
+			background-attachment: fixed;
+			animation: none;
+		}
 	}
 </style>
